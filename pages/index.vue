ff9b0b881bf1f06231780937de54d774c95a9ffc
shvam0000 <shvam0000@gmail.com>
2022-12-26 20:49:55 +0530

    WIP: add marker on map

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,50 @@
 <template>
-  <div id="map" class="v-screen h-screen"></div>
+  <div id="map" class="v-screen h-screen">
+    <div id="marker"></div>
+  </div>
 </template>
 
 <script>
 import mapboxgl from "mapbox-gl";
 
 export default {
+  data() {
+    return {
+      marker: null,
+    };
+  },
+  methods: {
+    createMap() {
+      mapboxgl.accessToken =
+        "pk.eyJ1Ijoic2h2YW0wMDAwIiwiYSI6ImNsYzI4bTB4OTA4aHUzeW85cGR5amU2d3EifQ.zro_-7iWwGVR0Q5VJ6OaaA";
+
+      // Creating map
+      const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-122.420679, 37.772537],
+        zoom: 15,
+      });
+
+      // Adding marker to the map
+      const el = document.getElementById("marker");
+      el.classList.add("markerggg");
+      el.style.backgroundColor = "black";
+      el.style.width = "50px";
+      el.style.height = "50px";
+
+      this.marker = new mapboxgl.Marker(el)
+        .setLngLat([-122.420679, 37.772537])
+        .setPopup(
+          new mapboxgl.Popup({ offset: 25 }).setHTML(
+            "<h3>San Francisco Airport</h3><p>SFO is the largest airport in the Bay Area.</p>"
+          )
+        )
+        .addTo(map);
+    },
+  },
   mounted() {
-    // Initialize the map
-    mapboxgl.accessToken =
-      "pk.eyJ1Ijoic2h2YW0wMDAwIiwiYSI6ImNsYzI4bTB4OTA4aHUzeW85cGR5amU2d3EifQ.zro_-7iWwGVR0Q5VJ6OaaA";
-    this.map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/streets-v11",
-    });
+    this.createMap();
   },
 };
 </script>
-
-<!-- <style>
-#map {
-  height: 100;
-  width: 100%;
-}
-</style> -->
